afe5d30f11c2a31988bd078707a2143d82faf653
Luis Lopez <luiscarloslj77777@gmail.com>
2020-11-07 14:08:03 -0600

    start

---
+++ javas.js
@@ -0,0 +1,13 @@
+
+
+let numero = 23;
+let texto = "23";
+let numero1 = 24;
+let texto1 = 24;
+document.write( numero == texto)
+document.write("<br>")
+document.write (numero === texto)
+document.write("<br>")
+document.write (numero !== texto)
+document.write("<br>")
+document.write (numero1 !== texto1)
